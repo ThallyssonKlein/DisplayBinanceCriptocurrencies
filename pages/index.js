43b70f88b4c07f904b4dd63aadf4498c2aacb57f
Thallysson Klein <thallyssonklein@gmail.com>
2021-04-29 08:31:57 +0000

    BasicConnectionWithWebsocket done

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
-export default function Home() {
-    return <h1>DisplayBinanceCriptocurrencies</h1>;
+import { useState } from 'react';
+import { WebSocket } from 'nextjs-websocket'
+
+export default function Home(){
+    const [ticker, setTicker] = useState("");
+
+    const handleData = data => {
+        setTicker(data);
+    }
+    
+    return <div>
+            <p>{ticker}</p>
+            <WebSocket url='wss://stream.binance.com:9443/ws/bnbbtc@ticker'
+                       onMessage={data => handleData(data)}
+            />
+    </div>;
 }
